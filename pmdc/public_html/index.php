729d16ffc33053cbbcde4a3912cf8797d733aa3d
aakram <akram.asad7@gmail.com>
2016-06-11 12:05:02 -0700

    new project setup

---
+++ pmdc/public_html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=MacRoman">
+<title>Minaal Rocks</title>
+</head>
+    <body>
+    <?php
+	echo "Site Under Construction";
+	?>
+    </body>
+</html>
