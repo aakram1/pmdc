45dec5a5e4a3e298046e487b73fc75890bb98b59
aakram <akram.asad7@gmail.com>
2016-06-11 12:26:01 -0700

    new setup

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,10 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<?php    
+    // load up your config file
+    require_once("/path/to/resources/config.php");
+     
+    require_once(TEMPLATES_PATH . "/header.php");
+?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=MacRoman">
