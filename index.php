86cbe9a4e7d8f79128d85ca419e073e230ab2788
aakram <akram.asad7@gmail.com>
2016-06-11 13:07:59 -0700

    remove config

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
-<?php    
-    // load up your config file
-    require_once("../resources/config.php");
-?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=MacRoman">
