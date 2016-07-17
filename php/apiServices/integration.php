ac3b290dcfe8fe3dfb5b770df712adac8c8ca7d1
aakram <akram.asad7@gmail.com>
2016-07-16 17:36:45 -0700

    fix

--- php/apiServices/integration.php
+++ php/apiServices/integration.php
@@ -1,26 +1,23 @@
 <?php
-include_once 'workflow/ApiRequests.php';
-include_once 'activities/CheapestFaresForDestinationActivity.php';
+include_once 'sabre/workflow/ApiRequests.php';
+include_once 'sabre/activities/CheapestFaresForDestinationActivity.php';
 
 $destination1 = $_GET["city"];
 $destination = "SFO";
 $countryCode = "US";
 $startdate = $_GET["startdate"];
 $enddate = $_GET["enddate"];
-$numGuests = $_GET["numGuests"];
+$numGuests = $_GET["numguests"];
 
-//$origin = filter_input(INPUT_POST, "countryCode");
-//$destination = filter_input(INPUT_POST, "destination");
-//$workflow = new Workflow(new CheapestFaresForDestinationActivity($countryCode, $destination));
-//$result = $workflow->runWorkflow();
-//ob_start();
-//var_dump($result);
-//$dump = ob_get_clean();
-//echo $dump;
+$workflow = new ApiRequests(new CheapestFaresForDestinationActivity($countryCode, $destination));
+$result = $workflow->runWorkflow();
+ob_start();
+var_dump($result);
+$dump = ob_get_clean();
+echo $dump;
 echo $destination1;
 echo $startdate;
 echo $enddate;
 echo $numGuests;
-//flush();
 
 ?>
