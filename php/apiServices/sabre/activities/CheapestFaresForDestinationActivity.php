0750c119cff6c8f58cbd481e0c32a39f9d78cac4
aakram <akram.asad7@gmail.com>
2016-07-16 18:17:30 -0700

    ok

--- php/apiServices/sabre/activities/CheapestFaresForDestinationActivity.php
+++ php/apiServices/sabre/activities/CheapestFaresForDestinationActivity.php
@@ -1,6 +1,6 @@
 <?php
-include_once 'workflow/Activity.php';
-include_once 'rest/RestClient.php';
+include_once 'sabre/workflow/Activity.php';
+include_once 'restServices/RestClient.php';
 class CheapestFaresForDestinationActivity implements Activity {
     private $destination, $countryCode;
     
