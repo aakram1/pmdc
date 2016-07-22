dd941dbcb90175aae4d1bf50fde3e3ad607a23ae
Omer <Omer@10.235.94.59>
2016-07-21 21:54:17 -0700

    new query

---
+++ php/apiServices/sabre/activities/TopCitiesFlightsEstimates.php
@@ -0,0 +1,38 @@
+<?php
+include_once 'sabre/workflow/Activity.php';
+include_once 'sabre/restServices/RestClient.php';
+class TopCitiesFlightsEstimates implements Activity {
+    private $destination, $origins, $startDate, $endDate;
+    
+    public function __construct($destination, $origins, $startDate, $endDate) {
+        $this->destination = $destination;
+        $this->origins = $origins;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+    }
+    
+    public function run(&$sharedContext) {
+        $sharedContext->addResult("origins", $this->origins);
+        $sharedContext->addResult("destination", $this->destination);
+        $sharedContext->addResult("startDate", $this->startDate);
+        $sharedContext->addResult("endDate", $this->endDate);
+        $call = new RestClient();
+        
+        foreach ($this->origins as $origin) {
+        	$result = $call->executeGetCall("/v1/shop/flights/", $this->getRequest($origin, $this->destination, $this->startDate, $this->endDate));
+        	$sharedContext->addResult($origin, $result);
+        }
+    }
+    
+    private function getRequest($origin, $destination, $departureDate, $returnDate) {
+        $request = array(
+        	"origin" => $origin,
+        	"destination" => $destination,
+        	"departuredate" => $departureDate,
+        	"returndate" => $returnDate
+        );
+        return $request;
+    }
+}
+
+?>
