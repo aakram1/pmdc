2a65973e9ae3909aae262ad9889579ee7c2b7590
oaftab <omeraftab235@gmail.com>
2016-07-21 22:05:44 -0700

    array

--- php/apiServices/integration.php
+++ php/apiServices/integration.php
@@ -15,7 +15,7 @@ $result = $workflow->runWorkflow();
 
 
 //ob_start();
-$jasonbhai = var_dump($result);
+$jasonbhai = array($result);
 //$dump = ob_get_clean();
 
 //$jasonbhai = json_decode($result);
