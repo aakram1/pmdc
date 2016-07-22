725616e94785161f6e7b18ed2943d6b4d6c7f528
oaftab <omeraftab235@gmail.com>
2016-07-21 22:16:02 -0700

    Asad

--- php/apiServices/integration.php
+++ php/apiServices/integration.php
@@ -13,15 +13,23 @@ $countryCode = "US";
 $workflow = new ApiRequests(new CheapestFaresForDestinationActivity($countryCode, $destination1));
 $result = $workflow->runWorkflow();
 
+$workflow2 = new ApiRequests(new TopCitiesFlightsEstimates("LAX",["SFO","ORD"], "12/12/2016", "12/16/2016"));
+$result2 = $workflow2->runWorkflow();
+
+ob_start();
+var_dump($result2);
+$dump2 = ob_get_clean();
+echo $dump2;
+
 
 //ob_start();
-$jasonbhai = print_r(json_decode($result));
+//$jasonbhai = print_r(json_decode($result));
 //$dump = ob_get_clean();
 
 //$jasonbhai = json_decode($result);
-for ($i = 0; $i <= 3; $i++) {
-	echo $jasonbhai[$i].'<br>';
-}
+//for ($i = 0; $i <= 3; $i++) {
+	//echo $jasonbhai[$i].'<br>';
+//}
 
 //$jasonbhai =;
 //var_dump(json_decode($dump, true));
