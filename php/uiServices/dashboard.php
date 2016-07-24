3126afe2e8a72d427f29f40b410734a886f63db3
aakram <akram.asad7@gmail.com>
2016-07-23 18:19:10 -0700

    k

--- php/uiServices/dashboard.php
+++ php/uiServices/dashboard.php
@@ -1,7 +1,5 @@
 <?php
-include_once 'php/apiServices/integration.php';
-
-session_start();
+include_once '/php/apiServices/integration.php';
 
 $destination = $_GET["city"];
 $startdate = $_GET["startdate"];
