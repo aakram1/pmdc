fc0efe7c5d07432d8339f0ac48ac19c72a684eda
Omer <Omer@10.0.1.12>
2016-07-26 00:55:33 -0700

    sendgrid password

--- php/email.php
+++ php/email.php
@@ -1,14 +1,16 @@
 <?php
 
 require '../vendor/autoload.php';
-$sendgrid = new SendGrid('app52008467@heroku.com', 'asvwlyiw5947');
-
+$sendgrid = new SendGrid('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');
+//$sendgrid = new SendGrid('app52008467@heroku.com', 'asvwlyiw5947');
+//
 $email = new SendGrid\Email();
+
 $email->addTo('omeraftab235@gmail.com')
-->setFrom('noreply@baarei.com')
-->setSubject('Pakistan ka matlab kya?')
-->setText('Pakistan Zindabad!')
-->setHtml('<strong>Pakistan Zindabad!</strong>');
+	->setFrom('noreply@baarei.com')
+	->setSubject('Pakistan ka matlab kya?')
+	->setText('Pakistan Zindabad!')
+	->setHtml('<strong>Pakistan Zindabad!</strong>');
 
 $sendgrid->send($email);
 
