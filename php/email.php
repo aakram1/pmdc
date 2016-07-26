115dd511135ea3d74b2322165c4cb789b941da81
Omer <Omer@10.0.1.12>
2016-07-26 01:28:41 -0700

    2

--- php/email.php
+++ php/email.php
@@ -26,7 +26,7 @@ $mail = new SendGrid\Mail($from, $subject, $to, $content);
 //$apiKey = getenv('Authorization: Bearer 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');
 //full access key: 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
 //$apiKey = getenv('Authorization: Bearer BiyrbV__TjmEWjN2qPvhMw');
-$apiKey = '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';
+$apiKey = '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';
 
 $sg = new \SendGrid($apiKey);
 $response = $sg->client->mail()->send()->post($mail);
