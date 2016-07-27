ab984245fe89e5a36399676a6d327221068b3e9e
oaftab <omeraftab235@gmail.com>
2016-07-26 22:18:48 -0700

    curl browser

--- php/apiServices/yellowpage.php
+++ php/apiServices/yellowpage.php
@@ -15,6 +15,8 @@ echo $url;
 $ch = curl_init();
 // set URL and other appropriate options
 $timeout = 5; // set to zero for no timeout
+//probably need to switch to a POST method
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
